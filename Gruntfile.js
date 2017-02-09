9654365fd82fd843ba6b7b8a86b8786c66791e31
Eren Senel <ernsnl@gmail.com>
2017-02-08 21:45:31 -0500

    Setting Environment

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        uglify: {
+            dynamic_mappings: {
+                files: [{
+                    expand: true, // Enable dynamic expansion.
+                    cwd: 'src/js/',
+                    src: ['*.js'], // Actual pattern(s) to match.
+                    dest: 'build/js/', // Destination path prefix.
+                    ext: '.min.js', // Dest filepaths will have this extension.
+                }, ]
+            }
+        },
+        copy: {
+            main: {
+                files: [{
+                        src: ['bower_components/jquery/dist/jquery.js'],
+                        dest: "src/js/jquery.js"
+                    },
+                    {
+                        src: ['bower_components/knockout/dist/knockout.js'],
+                        dest: "src/js/knockout.js"
+                    },
+                    {
+                        src: ['bower_components/bootstrap/dist/css/bootstrap.css'],
+                        dest: "src/css/bootstrap.css"
+                    },
+                    {
+                        src: ['bower_components/font-awesome/css/font-awesome.css'],
+                        dest: 'src/css/font-awesome.css'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/font-awesome/fonts/*'],
+                        dest: 'fonts/',
+                        filter: 'isFile'
+                    }
+                ]
+            },
+        },
+        cssmin: {
+            dynamic_mappings: {
+                files: [{
+                    expand: true, // Enable dynamic expansion.
+                    cwd: 'src/css/',
+                    src: ['*.css'], // Actual pattern(s) to match.
+                    dest: 'build/css/', // Destination path prefix.
+                    ext: '.min.js', // Dest filepaths will have this extension.
+                }, ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy')
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['copy', 'uglify', 'cssmin']);
+};
